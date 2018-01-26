55e4aedda86f9071a667ed78014e1111e8285140
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro.local>
2018-01-25 19:43:52 -0800

    Completed Cal2D

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -3,98 +3,6 @@ import java.util.*;
 public class Main {
 
     /**
-     * Defines a shape
-     */
-    private interface Shape {
-        Double getArea();
-    }
-
-    /**
-     * Defines a Non Circular Shape
-     */
-    private interface NonCircularShape {
-        Double getPerimeter();
-    }
-
-    /**
-     * Defines a Rectangle
-     */
-    private class Rectangle implements Shape, NonCircularShape {
-
-        private Double mLength;
-        private Double mWidth;
-
-        /**
-         * Calculates area of Rectangle
-         *
-         * @return area of the Rectangle
-         */
-        @Override
-        public Double getArea() {
-            return mLength * mWidth;
-        }
-
-        /**
-         * Gets the perimeter of Rectangle
-         * @return perimeter
-         */
-        public Double getPerimeter() {
-            return 2 * (mLength + mWidth);
-        }
-    }
-
-    /**
-     * Defines a Circle
-     */
-    private class Circle implements Shape {
-
-        private Double mRadius;
-
-        /**
-         * Calculates area of Circle
-         * @return area of the Circle
-         */
-        @Override
-        public Double getArea() {
-            return Math.PI;
-        }
-
-        /**
-         * Gets the circumference of a Circle
-         * @return circumference
-         */
-        public Double getCircumference() {
-            return 2 * Math.PI * mRadius;
-        }
-    }
-
-    /**
-     * Defines a Triangle
-     */
-    private class Triangle implements Shape, NonCircularShape {
-
-        private Double mSideA;
-        private Double mSideB;
-        private Double mSideC;
-
-        /**
-         * Gets the area of a Triangle
-         * @return area of the Triangle
-         */
-        @Override
-        public Double getArea() {
-            Double S = (mSideA + mSideB + mSideC) / 2;
-            Double A = Math.sqrt(S * (S - mSideA) * (S - mSideB) * (S - mSideC));
-            return A;
-        }
-
-        @Override
-        public Double getPerimeter() {
-            return mSideA + mSideB + mSideC;
-        }
-    }
-
-    /**
      * This static helper method prevents less error
      * in printing input message
      * @param _str
@@ -108,16 +16,123 @@ public class Main {
      * less error in printing output message
      */
     static void clearPrintOutput(String _str, Double _output) {
-        System.out.print(_str + ": " + _output.toString());
+        System.out.println(_str + ": " + _output.toString());
     }
 
     /**
      * This object holds all the 2D states
      */
-    private class Calc2d {
+    private static class Calc2d {
+
+        //
+        // Cal2d object dependencies
+        //
+
+        /**
+         * Defines a shape
+         */
+        private interface Shape {
+            Double getArea();
+        }
+
+        /**
+         * Defines a Non Circular Shape
+         */
+        private interface NonCircularShape {
+            Double getPerimeter();
+        }
+
+        /**
+         * Defines a Rectangle
+         */
+        private class Rectangle implements Shape, NonCircularShape {
+
+            public Double mLength;
+            public Double mWidth;
+
+            /**
+             * Calculates area of Rectangle
+             *
+             * @return area of the Rectangle
+             */
+            @Override
+            public Double getArea() {
+                return mLength * mWidth;
+            }
+
+            /**
+             * Gets the perimeter of Rectangle
+             * @return perimeter
+             */
+            public Double getPerimeter() {
+                return 2 * (mLength + mWidth);
+            }
+        }
+
+        /**
+         * Defines a Circle
+         */
+        private class Circle implements Shape {
+
+            public Double mRadius;
+
+            /**
+             * Calculates area of Circle
+             * @return area of the Circle
+             */
+            @Override
+            public Double getArea() {
+                return Math.PI;
+            }
+
+            /**
+             * Gets the circumference of a Circle
+             * @return circumference
+             */
+            public Double getCircumference() {
+                return 2 * Math.PI * mRadius;
+            }
+        }
+
+        /**
+         * Defines a Triangle
+         */
+        private class Triangle implements Shape, NonCircularShape {
+
+            public Double mSideA;
+            public Double mSideB;
+            public Double mSideC;
+
+            /**
+             * Gets the area of a Triangle
+             * @return area of the Triangle
+             */
+            @Override
+            public Double getArea() {
+                Double S = (mSideA + mSideB + mSideC) / 2;
+                Double A = Math.sqrt(S * (S - mSideA) * (S - mSideB) * (S - mSideC));
+                return A;
+            }
+
+            @Override
+            public Double getPerimeter() {
+                return mSideA + mSideB + mSideC;
+            }
+        }
+
+        // ********* End of Cal2D dependencies ********** //
+
+        // Cal2D member variables
+
         private Rectangle mRectangle;
         private Circle mCircle;
         private Triangle mTriangle;
+
+        Calc2d() {
+            mRectangle = new Rectangle();
+            mCircle = new Circle();
+            mTriangle = new Triangle();
+        }
 
         /**
          * Runs the Calc2D
@@ -151,6 +166,7 @@ public class Main {
             input = scanner.nextDouble();
             mTriangle.mSideC = input;
 
+            System.out.println();
             clearPrintOutput("Area of rectangle", mRectangle.getArea());
             clearPrintOutput("Area of circle", mCircle.getArea());
             clearPrintOutput("Perimeter of rectangle", mRectangle.getPerimeter());
@@ -163,5 +179,6 @@ public class Main {
     public static void main(String args[]) {
 
         Calc2d cal2d = new Calc2d();
+        cal2d.run();
     }
 }
