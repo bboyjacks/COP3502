4d019c523b6ace0b7e88213a2186cd8168fca7d8
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro.local>
2018-03-01 20:16:09 -0800

    Completed numOfDigits

--- RLE/src/RLE.java
+++ RLE/src/RLE.java
@@ -5,7 +5,7 @@ public class RLE {
 
     public static int numOfDigits(int num)
     {
-        return 0;
+        return (int)(Math.log10(num) + 1);
     }
 
     public static char[] toCharArray(int charCount, char strchar)
@@ -33,10 +33,20 @@ public class RLE {
         return new char[][] { {'a', 'b'}, {'a', 'b'}};
     }
 
+    public static void assertM(boolean condition)
+    {
+        if (condition)
+            System.out.println("Assert Passed!");
+        else
+            System.out.println("Failed!");
+    }
+
     public static void main(String[] args)
     {
         // Scanner scanner = new Scanner(System.in);
-        Scanner scanner = new Scanner(new ByteArrayInputStream("1 2 a 3 45 word 20".getBytes()));
+
+        // WORKING INPUT TESTED
+/*        Scanner scanner = new Scanner(new ByteArrayInputStream("1 2 a 3 45 word 20".getBytes()));
         while(scanner.hasNext())
         {
             if (scanner.hasNextInt())
@@ -46,6 +56,10 @@ public class RLE {
                 System.out.println("Error! Invalid input. Please enter an integer from 1-4");
                 scanner.next();
             }
-        }
+        }*/
+
+        assertM(numOfDigits(2) == 1);
+        assertM(numOfDigits(12314231) == 8);
+        assertM(numOfDigits(20) == 2);
     }
 }
