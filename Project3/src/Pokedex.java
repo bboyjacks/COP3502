ad17292d37dbb605a4564acb267473dcfb0249f4
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro.local>
2018-03-23 03:24:14 -0700

    Added implements Pokedex

--- Project3/src/Pokedex.java
+++ Project3/src/Pokedex.java
@@ -1,2 +1,28 @@
-public class Pokedex {
+public class Pokedex implements PokedexInterface {
+
+
+    @Override
+    public String[] listPokemon() {
+        return new String[0];
+    }
+
+    @Override
+    public boolean addPokemon(String species) {
+        return false;
+    }
+
+    @Override
+    public int[] checkStats(String species) {
+        return new int[0];
+    }
+
+    @Override
+    public void sortPokedex() {
+
+    }
+
+    @Override
+    public boolean evolvePokemon(String species) {
+        return false;
+    }
 }
